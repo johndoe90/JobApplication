3abf86e269d388099657770366e8188ff8628fbf
Phillip Friedrich <friedrich.phillip@gmail.com>
2014-05-08 11:07:58 +0200

    sixth commit

--- app/scripts/controllers/InfoController.js
+++ app/scripts/controllers/InfoController.js
@@ -4,13 +4,15 @@ angular
 	.module('jobApplication.controllers')
 	.controller('InfoController', ['$scope', function($scope){
 		$scope.infos = [{
-			title: 'Willkommen',
+			title: 'Willkommen,',
 			imageUrls: [],
 			imageTexts: [],
-			body: 'zu meinem etwas anderen Bewerbungsschreiben.'
+			body: 'zu meinem etwas anderen Bewerbungsschreiben. Hier haben Sie die Möglichkeit einiges Wissenswerte über mich zu erfahren. Dazu müssen Sie nur auf einen der <a href="#" class="query">#Links</a> (links oben) klicken und Sie werden weitere Informationen zu diesem Punkt erhalten. <br> Um die Wörter, welche die einzelenen Elemente miteinander verbinden, besser lesen zu können, ist jedes Element beliebig verschiebbar (linke Maustaste gedrückt halten). Außerdem kann man jedes Element anklicken um zusätzliche Infos darüber zu erfahren. <br> Viel Spaß beim stöbern!'
 		}];
 
 		/*$scope.remove = function(index) {
 			$scope.infos.splice(index, 1);
 		};*/
+
+		console.log(navigator.sayswho);
 	}]);
